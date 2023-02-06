0f95d23cd7663d1a0458c420b1aee7058cc3b708
Charles-Spencer Coleman <spencole@gmail.com>
2023-02-06 21:18:06 +0000

    Task 03 submission added

---
+++ T03/hello_world.py
@@ -0,0 +1,13 @@
+#You first computer program
+
+#request user for name and store in variable name
+name = input ("Please enter your name: ")
+#print out the user's name
+print("Your Name is " + name)
+#request user for age and store in variable age
+age = input("Please enter your age: ")
+#print out the user's age and Hello World
+#f allows us to access the variable in the string
+print (f"Your age is:{age} ")
+print("Hello World")
+
---
+++ T03/menu.py
@@ -0,0 +1,13 @@
+#list menu items for customer
+print("Fish & Chip, Nachos, Mac & Cheese, Burger, Chicken & Chips")
+#Ask customer to enter 3 food items
+print("You can order 3 items from the menu.")
+first_order = input("Please Enter first Item: ")
+second_order = input("Please enter your second order: ")
+third_order = input("Please enter your third order: ")
+#print order confirmation
+print("Thank you for your order")
+print("TOrder confirmation! You have ordered:")
+print(first_order)
+print(second_order)
+print(third_order)
